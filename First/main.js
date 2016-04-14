002454ac81860e96d22d0c541a97e152fde447b2
jufianto <jufiantohendri@gmail.com>
2016-04-15 04:08:54 +0700

    First Learn

---
+++ First/main.js
@@ -0,0 +1,20 @@
+paper.install(window);
+paper.setup(document.getElementById('mainCanvas'));
+
+/*var c;
+for(var x=25; x<400; x+=50) {
+for(var y=25; y<400; y+=50) {
+c = Shape.Circle(x, y, 20);
+c.fillColor = 'green';
+}
+}*/
+
+var c = Shape.Circle(200, 200, 80);
+c.fillColor = 'black';
+var text = new PointText(200, 200);
+text.justification = 'center';
+text.fillColor = 'white';
+text.fontSize = 20;
+text.content = 'hello world';
+
+paper.view.draw();
